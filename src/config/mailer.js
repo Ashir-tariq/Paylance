04794519ad55254deee62666970dbca8a4798581
Muhammad Ashir <Muhammad Ashir>
2026-06-02 23:43:23 +0500

    Fix mailer IPv4

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,21 +1,14 @@
-// src/config/mailer.js
-// -----------------------------------------------
-// Yeh file Nodemailer setup karti hai
-// Flask mein tha: smtplib.SMTP_SSL("smtp.gmail.com")
-// Node mein same kaam Nodemailer karta hai
-// -----------------------------------------------
-
 const nodemailer = require('nodemailer');
 
-// "Transporter" matlab email bhejna ka darwaza
-// Flask mein: server = smtplib.SMTP_SSL(...)
-//             server.login(sender_email, app_password)
 const transporter = nodemailer.createTransport({
-    service: 'gmail',           // Gmail use kar raha hai
-    auth: {
-        user: process.env.SENDER_EMAIL,   // .env se aata hai
-        pass: process.env.APP_PASSWORD    // .env se aata hai
-    }
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.APP_PASSWORD,
+  }
 });
 
 module.exports = transporter;
